8cf658a58fa836690985b8607d1eb5f6c22bad17
Sinyeol An <soundlake@gmail.com>
2014-02-25 17:52:10 +0900

    game more

--- html/game_arti/controller.js
+++ html/game_arti/controller.js
@@ -1,37 +1,44 @@
-var artiGame = angular.module('artiGame', []);
+var artiGame = angular.module('artiGame', ['ngRoute']);
 
-artiGame.controller('artiGameController', function ($scope) {
+artiGame.service('artiGameService', function () {
     "use strict";
 
-    /*
-    var style = {};
-    style.top = Math.floor(Math.random() * 400);
-    style.left = Math.floor(Math.random() * 1200);
-    $scope.artiGameStyle = style;
-    */
-    var num = 0;
-    $scope.setPosition = function () {
-        console.log(++num);
-        var style = {};
-        style.top = Math.floor(Math.random() * 400) + 200;
-        style.left = Math.floor(Math.random() * 1200);
-        return style;
+    return {
+        data: {
+            'words': ['엄지', '엄마', '가방', '바위', '전화', '그릇', '검사', '발성', '바다'],
+            'points': [0, 50, 100, 150, 200, 250],
+            'imgs': ['../img/3_arti/game1/bigCircle01.png']
+        },
+        getBigCircle: function () {
+            var circle = {};
+            circle.img = this.data.imgs[Math.floor(Math.random() * this.data.imgs.length)];
+            circle.word = this.data.words[Math.floor(Math.random() * this.data.words.length)];
+            circle.point = this.data.points[Math.floor(Math.random() * this.data.points.length)];
+            return circle;
+        },
+        setPosition: function () {
+            var style = {};
+            style.top = Math.floor(Math.random() * 400) + 200;
+            style.left = Math.floor(Math.random() * 300) + 50 + Math.floor(Math.random() * 2) * 900;
+            return style;
+        },
+        getPosition: function () {
+            var a = [];
+            for (var i in [0, 1, 2, 3]) {
+                a[i] = this.setPosition();
+            }
+            return a;
+        }
     };
+});
 
-    $scope.loadData = function () {
-        var data = {
-                'words': ['엄지', '엄마', '가방', '바위', '전화', '그릇', '검사', '발성', '바다'],
-                'points': [0, 50, 100, 150, 200, 250],
-                'imgs': ['../img/3_arti/game1/bigCircle01.png']
-            },
-            circle = {};
+artiGame.controller('artiGameController', ['$scope', 'artiGameService', function ($scope, artiGameService) {
+    "use strict";
 
-        circle.img = data.imgs[Math.floor(Math.random() * data.imgs.length)];
-        circle.word = data.words[Math.floor(Math.random() * data.words.length)];
-        circle.point = data.points[Math.floor(Math.random() * data.points.length)];
-
-        $scope.bigCircle = circle;
+    $scope.loadData = function ($event, val) {
+        $scope.bigCircle = artiGameService.getBigCircle();
+        $scope.position = artiGameService.getPosition();
     };
 
     $scope.loadData();
-});
+}]);
