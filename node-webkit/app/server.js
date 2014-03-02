673755eb56733cdea6f1ccd96013c0975a145f4d
Sinyeol An <soundlake@gmail.com>
2014-03-02 17:45:51 +0900

    h

--- node-webkit/app/server.js
+++ node-webkit/app/server.js
@@ -1,24 +1,39 @@
-var express = require("express"),
-    path = require("path"),
-    nedb = require('nedb'),
-    databaseUrl = "db/items.db";
+var express = require('express'),
+    routes = require('./routes'),
+    utils = require('./utils'),
+    http = require('http'),
+    path = require('path'),
+    nedb = require('nedb');
 
 var db = {
-    items: new nedb({ filename: databaseUrl, autoload: true })
+    items: new nedb({ filename: 'db/items.db', autoload: true })
 };
 
 var app = express();
 
 app.configure(function () {
     app.set('port', process.env.PORT || 4972);
+    app.set('views', __dirname + '/public/views');
+    app.set('view engine', 'jade');
     app.use(express.logger('dev'));
-    app.use(express.bodyParser()),
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.router);
     app.use(express.static(path.join(__dirname, 'public')));
 });
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+app.get('/', routes.index);
+app.get('/users', user.list);
 
 app.get('/api', function (req, res) {
     res.send('API is running');
 });
+
 
 app.get('/items', function (req, res) {
     db.items.find({}, function(err, result) {
@@ -50,5 +65,6 @@ app.delete('/items/:id', function (req, res) {
     });
 });
 
-app.listen(app.get('port'));
-console.log('Server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
